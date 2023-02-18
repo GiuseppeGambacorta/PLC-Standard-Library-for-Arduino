1f49c59633998127c5e63410d14dc9cf846d2fbd
Giuseppe <giuseppe@pop-os.localdomain>
2023-02-18 23:16:59 +0100

    ciao

--- Readme.md
+++ Readme.md
@@ -1,8 +1,10 @@
-Eliminare file inutili
+Eliminare file inutili 
 
-Aggiungere timer
+
+Aggiungere timer, aggiunegere funzione clock timer on e off
 completare analogico, fare filtraggio segnale, creare funzione map nostra per ingresso
-fare uscita analogica con map annessa 
+fare uscita analogica con map annessa // inutile
+
 Fare esempi
 
 Presentazione IEC library su Readme e su youtube
--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,35 @@
 
 
 extern Time time;
+typedef struct message
+{
+  int count[2];
+   
+}message;
 
-String ciao;
+
+String MessageToSend(message *var){
+  String sendmessage;
+  unsigned int i;
+  for ( i = 0; i < sizeof(var->count) / sizeof(var->count[0]) -1; i++)
+  {
+    sendmessage.concat((String)var->count[i]);
+    sendmessage.concat("!");
+   
+  }
+  
+  sendmessage.concat((String)var->count[i]);
+  sendmessage.concat("\n");
+  return sendmessage;
+}
+
+
+message prova;
+int array[20];
+float average;
+unsigned int i;
+unsigned int x;
+AnalogicInput sensore = AnalogicInput(14);
 
 void setup() {  
   Serial.begin(9600);
@@ -13,13 +40,36 @@ void setup() {
 
 void loop() {
     time.Count();
-  
+
+    /*
+    
+    for ( i = 0; i < sizeof(prova.count) / sizeof(prova.count[0]); i++)
+    {
+       prova.count[i] = time.GetSeconds() + i;
+    }
+
+    */
+
    
-    ciao.concat((String)time.GetActualSeconds());
-    ciao.concat(".");
-    ciao.concat("53");
-    Serial.println(ciao);
-    ciao = "";
+   if (x >= 20){
+     x = 0;
+   }
+   array[x] = sensore.ReadRealValue();
+   x++;
+    average = 0;
+    for ( i = 0; i < 20; i++)
+    {
+       average += (float)array[i] / 20.0;
+       
+    }
+    
+    prova.count[0] = sensore.ReadRealValue();
+    prova.count[1] = average ;
+    
+    Serial.println(MessageToSend(&prova));
+    //Serial.println(average);
+    delay(200);
+    
     
     
    
