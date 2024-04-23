4a410bfe2af430e257e0cd985297ed789973f6f6
Whatsm97 <101347069+GiuseppeGambacorta@users.noreply.github.com>
2024-04-24 00:45:21 +0200

    updated io.cpp formatting

--- plc/io.cpp
+++ plc/io.cpp
@@ -1,41 +1,33 @@
 #include "io.h"
 #include <Arduino.h>
 
-
-Input:: Input(int input_pin, bool normalestate = NORMALY_OPEN){
+Input:: Input(int input_pin, bool normalState = NORMALY_OPEN){
    this->_pin = input_pin;
-   this->_normalState = normalestate;
+   this->_normalState = normalState;
    pinMode(this->_pin,INPUT);
-
-  }
+}
 
 bool Input:: isActive(){
-
     if(this->_normalState == NORMALY_OPEN){
-      return digitalRead(this->_pin);
-    }else if (this->_normalState == NORMALY_CLOSED){
-      return !digitalRead(this->_pin);
+        return digitalRead(this->_pin);
+    } else if (this->_normalState == NORMALY_CLOSED){
+        return !digitalRead(this->_pin);
     }
-      
-  }
-
-
+}
 
 Output::Output(int input_pin) {
-     
-      this->_pin = input_pin;
-      pinMode(this->_pin, OUTPUT);
-      
-    }
+    this->_pin = input_pin;
+    pinMode(this->_pin, OUTPUT);
+ }
 
 void Output::on() {
-      digitalWrite(this->_pin, HIGH);
-    }
+    digitalWrite(this->_pin, HIGH);
+}
 
 void Output::off() {
-      digitalWrite(this->_pin, LOW);
-    }
+    digitalWrite(this->_pin, LOW);
+}
 
 bool Output::isActive(){
-      return digitalRead(this->_pin);
-    }
+    return digitalRead(this->_pin);
+}
