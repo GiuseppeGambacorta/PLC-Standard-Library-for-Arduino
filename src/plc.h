a37df7059a17d9b35f1678396ae9e4dfaa8fa3d5
Giuseppe Gambacorta <gambacortagiuseppe@outlook.it>
2024-04-24 21:24:44 +0200

    created src folder

---
+++ src/plc.h
@@ -0,0 +1,16 @@
+#ifndef PLC_h
+#define PLC_h
+
+
+#ifndef NOTIMERS
+#include "plc/timers.h"
+#endif
+
+#include "plc/io.h"
+#include "plc/analog.h"
+#include "plc/trig.h"
+#include "plc/counters.h"
+
+
+
+#endif
